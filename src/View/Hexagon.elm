d19657d94455e2ffa560b195ad602cd6e5f29151
Joris <joris.guyonvarch@gmail.com>
2015-08-28 12:20:22 +0200

    Set the hexagon to be straight

--- src/View/Hexagon.elm
+++ src/View/Hexagon.elm
@@ -11,8 +11,7 @@ corners center size = List.map (corner center size) [0..5]
 
 corner : Position -> Float -> Int -> Position
 corner center size i =
-  let angleDeg = 60 * i + 20
-      angleRad = pi / 180 * angleDeg
-  in  { x = center.x + size * (cos angleRad)
-      , y = center.y + size * (sin angleRad)
+  let angle = degrees (toFloat (60 * i))
+  in  { x = center.x + size * (cos angle)
+      , y = center.y + size * (sin angle)
       }
