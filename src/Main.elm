8bff7aca9fc47cb98a8fd9f06236bb82ea4d10ff
Joris <joris.guyonvarch@gmail.com>
2015-08-28 10:47:47 +0200

    Moving the view to another file

--- src/Main.elm
+++ src/Main.elm
@@ -3,37 +3,8 @@ module Main
   ) where
 
 import Html exposing (Html)
-import Svg exposing (..)
-import Svg.Attributes exposing (..)
 
-import Constant.Size as Size
-import Constant.Color as Color
+import View exposing (board)
 
 main : Html
-main =
-  svg
-    [ width (toString Size.boardWidth)
-    , height (toString Size.boardHeight)
-    ]
-    [ background
-    , title
-    ]
-
-background : Svg
-background =
-  rect
-    [ width (toString Size.boardWidth)
-    , height (toString Size.boardHeight)
-    , fill Color.background
-    ]
-    []
-
-title : Svg
-title =
-  text'
-    [ x "5"
-    , y (toString (Size.title + 5))
-    , fill Color.title
-    , fontSize (toString Size.title)
-    ]
-    [ text "Hackzagons" ]
+main = board
---
+++ src/View.elm
@@ -0,0 +1,39 @@
+module View
+  ( board
+  ) where
+
+import Html exposing (Html)
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
+
+import Constant.Size as Size
+import Constant.Color as Color
+
+board : Html
+board =
+  svg
+    [ width (toString Size.boardWidth)
+    , height (toString Size.boardHeight)
+    ]
+    [ background
+    , title
+    ]
+
+background : Svg
+background =
+  rect
+    [ width (toString Size.boardWidth)
+    , height (toString Size.boardHeight)
+    , fill Color.background
+    ]
+    []
+
+title : Svg
+title =
+  text'
+    [ x "5"
+    , y (toString (Size.title + 5))
+    , fill Color.title
+    , fontSize (toString Size.title)
+    ]
+    [ text "Hackzagons" ]
